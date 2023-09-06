459c39def9c28e31a489e6b2ea0d2cf3de11f0d6
guemplen <guemplen@gmail.com>
2023-09-07 00:16:56 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,21 +7,20 @@ const inputDate = document.getElementById('datetime-picker');
 const startButton = document.querySelector('[data-start]');
 
 startButton.setAttribute('disabled', 'disabled');
-const currentDate = new Date();
+let currentDate = new Date();
 
 flatpickr(inputDate, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose: function (selectedDates) {
+  onClose(selectedDates) {
     const selectedDate = selectedDates[0];
     if (selectedDate < currentDate) {
       window.alert('Please choose a date in the future');
     } else {
       startButton.removeAttribute('disabled');
     }
-    //console.log(selectedDates[0]);
   },
 });
 
